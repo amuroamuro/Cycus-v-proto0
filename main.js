ceae2be8c844fbdc4561f5ab9c5b57629b3329ba
amuroamuro <mbhstar1206@gmail.com>
2024-11-13 17:12:06 +0900

    이렇게!!

--- main.js
+++
@@ -1,4 +0,0 @@
-//dbSet
-dbSet();
-
-indexedDB.deleteDatabase('myDataBase');
--- modules/DB/DBset.js
+++ modules/DB/DBset.js
@@ -1,19 +1,17 @@
-function dbSet() {
-    if(!window.indexedDB) {
+if(!window.indexedDB) {
         //시발 지원안된다고 말하셈.
-    } else{
-        const Req = indexedDB.open("DB", 2);
-        let db;
-        Req.addEventListener('success', function(event) {
-            db = event.target.result;
-        });
-        Req.addEventListener('error', function(event) {
-            var error = event.target.error;
-            alert("Holy shit, ERROR : ", error.name);
-        });
-        Req.addEventListener('upgradeneeded', function(event){
-            db = event.target.result;
-            db.createObjectStore('CycusProto0', {keyPath:'type', autoIncrement:false});
-        });
-    }
+} else{
+    const Req = indexedDB.open("DB", 2);
+    let db;
+    Req.addEventListener('success', function(event) {
+        db = event.target.result;
+    });
+    Req.addEventListener('error', function(event) {
+        var error = event.target.error;
+        alert("Holy shit, ERROR : ", error.name);
+    });
+    Req.addEventListener('upgradeneeded', function(event){
+        db = event.target.result;
+        db.createObjectStore('CycusProto0', {keyPath:'type', autoIncrement:false});
+    });
 }
